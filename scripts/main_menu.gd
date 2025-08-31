5bb54950570572b81024fd110bedd83a423086e0
Amr <amr@programmer.net>
2025-08-31 09:51:09 +0300

    rename `Quit` -> `Exit` in main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,7 +4,7 @@ class_name MainMenu
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		on_quit()
+		on_exit()
 
 
 func on_start() -> void:
@@ -15,5 +15,5 @@ func on_options() -> void:
 	pass # Replace with function body.
 
 
-func on_quit() -> void:
+func on_exit() -> void:
 	get_tree().quit()

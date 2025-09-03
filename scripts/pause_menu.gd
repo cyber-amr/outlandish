8d7d67d3f5488fc4412b57bcc4bdebf174b4f5aa
Amr <amr@programmer.net>
2025-09-03 10:40:58 +0300

    close pause menu on `pause` action

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -2,6 +2,11 @@ extends Node
 class_name PauseMenu
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		on_continue()
+
+
 func on_continue() -> void:
 	queue_free()
 
